a856ff5fcdd7382b7b6f48c77fe909694b9044fa
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-17 14:50:01 +0200

    move helpers and units to common

--- src/common/helpers.ts
+++ src/common/helpers.ts
@@ -1,7 +1,22 @@
-export const ONE_HOUR_IN_MS = 3.6e+6;
+export const ONE_HOUR_IN_MS = 3.6e6;
+
+export interface MeasureType<Unit> {
+  value: number;
+  unit: Unit;
+}
+
+export type Sex = 'MALE' | 'FEMALE';
+
+export type Mass = 'KG' | 'G';
+
+export type Length = 'M' | 'CM';
+
+export type MealSize = 'NONE' | 'SMALL' | 'LARGE';
+
+export type Time = 'HOURS' | 'MS';
 
 export const toDecimalPlaces = (n: number, places = 3): number => {
-  return parseFloat((n).toFixed(places));
+  return parseFloat(n.toFixed(places));
 };
 
 export const elapsedTimeFromMsToHours = (elapsedTime: number): number => {
@@ -10,4 +25,8 @@ export const elapsedTimeFromMsToHours = (elapsedTime: number): number => {
 
 export const elapsedHoursToMs = (elapsedTime: number): number => {
   return elapsedTime * ONE_HOUR_IN_MS;
+};
+
+export const sum = (series: number[]): number => {
+  return series.reduce((a: number, b: number) => a + b, 0);
 };
