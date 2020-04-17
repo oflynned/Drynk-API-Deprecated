9d041e84e576c45b9434d16170ddfdce9962fb96
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-17 12:32:13 +0200

    add bac time to sober and max effects

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -5,10 +5,29 @@ import { User } from '../models/user.model';
 // based on this research paper
 // https://staff.fnwi.uva.nl/a.j.p.heck/Research/art/ICTMT8_2.pdf
 export class Session {
-  private static widmark(drink: Drink, user: User): number {
-    // BAC = (ethanol of drink in grams / (widmark factor * weight in g)) - (metabolism rate * time in hours since drink)
-    const bloodAlcoholEffect = ((drink.ethanolGrams() / (user.widmarkConstant * user.weight)) * 100) - (user.metabolismRate * drink.timeSinceDrink('HOURS').elapsedTime);
-    return Math.max(0, bloodAlcoholEffect);
+  private readonly PEAK_BAC_EMPTY_STOMACH = 45;
+  private readonly PEAK_BAC_NORMAL_STOMACH = 60;
+  private readonly PEAK_BAC_FULL_STOMACH = 75;
+
+  private static widmark(drink: Drink, user: User, decay: boolean = true): number {
+    // BAC = (D / (r * W) * 100) - (β * t)
+    // BAC = (ethanol of drink in grams / (widmark factor * weight in g) * 100) - (metabolism rate * time in hours since drink)
+    const elapsedTime = decay ? drink.timeSinceDrink('HOURS').elapsedTime : 0;
+    const bloodAlcoholContentFromDrink = ((drink.ethanolGrams() / (user.widmarkConstant * user.weight('G').value)) * 100) - (user.metabolismRate * elapsedTime);
+    return Math.max(0, bloodAlcoholContentFromDrink);
+  }
+
+  private static widmarkTimeToSober(drink: Drink, user: User, decay: boolean): number {
+    const peakBac = Session.widmark(drink, user, decay);
+    return peakBac / user.metabolismRate;
+  }
+
+  private static wagner(drink: Drink, user: User): number {
+    return 0;
+  }
+
+  private static pieters(drink: Drink, user: User): number {
+    return 0;
   }
 
   async calculateDrinkSeries(): Promise<object[]> {
@@ -24,6 +43,7 @@ export class Session {
       //      or over the drinks that are only affecting the user in the current session
       return {
         intake: {
+          name: drink.toJson().drinkName,
           volume: `${drink.toJson().volume} ml`,
           abv: `${drink.toJson().abv}%`,
           abvPercentage: drink.toJson().abv / 100,
@@ -34,7 +54,14 @@ export class Session {
           ...timeElapsedSinceDrink
         },
         bloodAlcoholContent: {
-          widmark: Session.widmark(drink, user)
+          widmark: {
+            maxBacEffect: Session.widmark(drink, user, false),
+            maxTimeToSober: Session.widmarkTimeToSober(drink, user, false),
+            currentBac: Session.widmark(drink, user, true),
+            currentTimeToSober: Session.widmarkTimeToSober(drink, user, true)
+          },
+          wagner: Session.wagner(drink, user),
+          pieters: Session.pieters(drink, user)
         }
       };
     });
