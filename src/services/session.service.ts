a9c7952b7f36c990f23d5cd8130a2ae13f376ca6
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-17 14:49:30 +0200

    add absorption/decay logic to widmark formula

--- src/services/session.service.ts
+++ src/services/session.service.ts
@@ -1,84 +1,139 @@
 import { Drink } from '../models/drink.model';
 import { Repository } from 'mongoize-orm';
 import { User } from '../models/user.model';
+import { MeasureType, sum, Time } from '../common/helpers';
+
+interface DrinkSeries {
+  timeToPeakBacEffect: MeasureType<Time>;
+  peakBacEffectWasPassed: boolean;
+  maxPredictedBac: number;
+  currentBac: number;
+}
 
 // based on this research paper
 // https://staff.fnwi.uva.nl/a.j.p.heck/Research/art/ICTMT8_2.pdf
 export class Session {
-  private readonly PEAK_BAC_EMPTY_STOMACH = 45;
-  private readonly PEAK_BAC_NORMAL_STOMACH = 60;
-  private readonly PEAK_BAC_FULL_STOMACH = 75;
+  private readonly user: User = new User();
 
-  private static widmark(drink: Drink, user: User, decay: boolean = true): number {
-    // BAC = (D / (r * W) * 100) - (β * t)
-    // BAC = (ethanol of drink in grams / (widmark factor * weight in g) * 100) - (metabolism rate * time in hours since drink)
-    const elapsedTime = decay ? drink.timeSinceDrink('HOURS').elapsedTime : 0;
-    const bloodAlcoholContentFromDrink = ((drink.ethanolGrams() / (user.widmarkConstant * user.weight('G').value)) * 100) - (user.metabolismRate * elapsedTime);
-    return Math.max(0, bloodAlcoholContentFromDrink);
+  private constructor() {}
+
+  private _drinkSeries: DrinkSeries[] = [];
+
+  get drinkSeries(): DrinkSeries[] {
+    return this._drinkSeries;
   }
 
-  private static widmarkTimeToSober(drink: Drink, user: User, decay: boolean): number {
-    const peakBac = Session.widmark(drink, user, decay);
+  static async getInstance() {
+    return new Session().buildSession();
+  }
+
+  // BAC = (D / (r * W) * 100) - (β * t)
+  private static widmark(
+    drink: Drink,
+    user: User,
+    decay: boolean = true
+  ): number {
+    // the max bac a drink has on someone is 0.75-1.25 hours
+    // this peak value is always accessible by getting the user's food state at the time
+    const waitTimeToPeakDrinkBac = user.timeToPeakDrinkEffect('HOURS').value;
+    const elapsedTime = decay
+      ? drink.timeSinceDrink('HOURS').value
+      : waitTimeToPeakDrinkBac;
+
+    // BAC = (ethanol of drink in grams / (widmark factor * weight in g) * 100) - (metabolism rate * time in hours since drink)
+    const maxBacEffect =
+      (drink.ethanolGrams() / (user.widmarkConstant * user.weight('G').value)) *
+      100;
+    if (elapsedTime < waitTimeToPeakDrinkBac) {
+      return maxBacEffect * elapsedTime;
+    }
+
+    // after 1 hour, excretion starts and the max bac observed from the drink starts to decay
+    const bloodAlcoholBacEffectFromDrink =
+      maxBacEffect -
+      user.metabolismRate * (elapsedTime - waitTimeToPeakDrinkBac);
+    return Math.max(0, bloodAlcoholBacEffectFromDrink);
+  }
+
+  private static widmarkTimeToSober(peakBac: number, user: User): number {
     return peakBac / user.metabolismRate;
   }
 
-  private static wagner(drink: Drink, user: User): number {
-    return 0;
+  isPeakPassed(drink: Drink, user: User): boolean {
+    return (
+      Date.now() - drink.toJson().createdAt.getTime() >
+      user.timeToPeakDrinkEffect('MS').value
+    );
   }
 
-  private static pieters(drink: Drink, user: User): number {
-    return 0;
-  }
-
-  async calculateDrinkSeries(): Promise<object[]> {
-    const user: User = new User();
-    const drinks: Drink[] = await Repository.with(Drink).findAll();
-
-    return drinks.map((drink: Drink): object => {
-      const timeElapsedSinceDrink = drink.timeSinceDrink('HOURS');
-      const ethanolGrams = drink.ethanolGrams();
-      const ethanolMls = drink.ethanolVolume();
-
-      // TODO refactor this as it is not needed aside from calculating the widmark hit for the series since the user was last sober
-      //      or over the drinks that are only affecting the user in the current session
-      return {
-        intake: {
-          name: drink.toJson().drinkName,
-          volume: `${drink.toJson().volume} ml`,
-          abv: `${drink.toJson().abv}%`,
-          abvPercentage: drink.toJson().abv / 100,
-          ethanolMls,
-          ethanolGrams
-        },
-        time: {
-          ...timeElapsedSinceDrink
-        },
-        bloodAlcoholContent: {
-          widmark: {
-            maxBacEffect: Session.widmark(drink, user, false),
-            maxTimeToSober: Session.widmarkTimeToSober(drink, user, false),
-            currentBac: Session.widmark(drink, user, true),
-            currentTimeToSober: Session.widmarkTimeToSober(drink, user, true)
-          },
-          wagner: Session.wagner(drink, user),
-          pieters: Session.pieters(drink, user)
-        }
-      };
-    });
-  }
-
-  async bloodAlcoholContent(): Promise<number> {
-    const drinkSeries = await this.calculateDrinkSeries();
-
-    if (drinkSeries.length === 0) {
+  bloodAlcoholContent(): number {
+    if (this._drinkSeries.length === 0) {
       return 0;
     }
 
-    return drinkSeries.map((item: any): number => item.bloodAlcoholContent.widmark)
-      .reduce((a: number, b: number) => a + b, 0);
+    return sum(this._drinkSeries.map((item: DrinkSeries) => item.currentBac));
+  }
+
+  async buildSession(): Promise<Session> {
+    this._drinkSeries = await this.calculateDrinkSeries();
+    return this;
+  }
+
+  async timeToSober(): Promise<number> {
+    if (this.drinkSeries.length === 0) {
+      return 0;
+    }
+
+    const drinksBeingAbsorbed = this.drinkSeries.filter(
+      (item: any) => !item.peakBacEffectWasPassed
+    );
+    const drinksBeingExcreted = this.drinkSeries.filter(
+      (item: any) => item.peakBacEffectWasPassed
+    );
+
+    const timeToFullAbsorption = sum(
+      drinksBeingAbsorbed.map(
+        (drinkSeries: DrinkSeries) => drinkSeries.timeToPeakBacEffect.value
+      )
+    );
+    const decayingBacUndigestedDrinks = sum(
+      drinksBeingAbsorbed.map(
+        (drinkSeries: DrinkSeries) => drinkSeries.maxPredictedBac
+      )
+    );
+    const decayingBacDigestedDrinks = sum(
+      drinksBeingExcreted.map(
+        (drinkSeries: DrinkSeries) => drinkSeries.currentBac
+      )
+    );
+
+    const maxPredictedBac =
+      decayingBacDigestedDrinks + decayingBacUndigestedDrinks;
+    return (
+      Session.widmarkTimeToSober(maxPredictedBac, this.user) +
+      timeToFullAbsorption
+    );
   }
 
   async isSober(): Promise<boolean> {
-    return await this.bloodAlcoholContent() === 0;
+    return (await this.bloodAlcoholContent()) === 0;
+  }
+
+  private async calculateDrinkSeries(): Promise<DrinkSeries[]> {
+    const user: User = new User();
+    const drinks: Drink[] = await Repository.with(Drink).findAll();
+
+    return drinks.map(
+      (drink: Drink): DrinkSeries => {
+        // TODO refactor this as it is not needed aside from calculating the widmark hit for the series since the user was last sober
+        //      or over the drinks that are only affecting the user in the current session
+        return {
+          timeToPeakBacEffect: drink.timeToPeakEffect(user, 'HOURS'),
+          peakBacEffectWasPassed: this.isPeakPassed(drink, user),
+          maxPredictedBac: Session.widmark(drink, user, false),
+          currentBac: Session.widmark(drink, user, true)
+        };
+      }
+    );
   }
 }
