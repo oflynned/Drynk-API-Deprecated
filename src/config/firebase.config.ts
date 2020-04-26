d68d36cf50b34664280dafdb9372656556e52814
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-26 17:45:13 +0200

    add default fallback to test env var issues

--- src/config/firebase.config.ts
+++ src/config/firebase.config.ts
@@ -1,7 +1,9 @@
 import admin, { AppOptions } from 'firebase-admin';
 
 // const serviceAccountConfig = () => process.env.FIREBASE_SERVICE_ACCOUNT;
-const serviceAccountConfig = () => require('../config/firebase-service-account.json');
+const serviceAccountConfig = () => {
+  return process.env.NODE_ENV === 'production' ? process.env.FIREBASE_SERVICE_ACCOUNT : require('../config/firebase-service-account.json');
+};
 
 export const firebaseConfig = (): AppOptions => {
   return {
