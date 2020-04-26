63b63aed4d7db461b294436f6aee872a72b36dee
Edmond O'Flynn <oflynned@tcd.ie>
2020-04-26 18:46:39 +0200

    fix session routes to provide a user

--- src/infrastructure/routes/session.route.ts
+++ src/infrastructure/routes/session.route.ts
@@ -4,21 +4,37 @@ import { User } from '../../models/user.model';
 import { ONE_DAY_IN_MS } from '../../common/helpers';
 import { SessionUser } from '../../models/session-user.model';
 import { Repository } from 'mongoize-orm';
+import { withFirebaseUser, withUser } from '../middleware/identity.middleware';
+import { AuthenticatedRequest } from '../middleware/authenticated.request';
 
 const routes = (): Router => {
   const router = Router();
 
   router.get(
     '/series',
-    async (req: Request, res: Response): Promise<void> => {
-      const users = await Repository.with(User).findAll({});
-      const user = new SessionUser('NONE', users[0]);
-      const session = await Session.getInstance(user);
+    withFirebaseUser,
+    withUser,
+    async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+      const sessionUser = new SessionUser('NONE', req.user);
+      const session = await Session.getInstance(sessionUser);
 
       const series = await session.buildTimeSeries({
         createdAt: { $gte: new Date(Date.now() - ONE_DAY_IN_MS) }
       } as object);
       res.status(200).json(series);
+    }
+  );
+
+  router.get(
+    '/events',
+    withFirebaseUser,
+    withUser,
+    async (req: AuthenticatedRequest, res: Response): Promise<void> => {
+      const sessionUser = new SessionUser('NONE', req.user);
+      const session = await Session.getInstance(sessionUser);
+
+      const eventEstimates = await session.estimateEventTimes({});
+      res.status(200).json(eventEstimates);
     }
   );
 
@@ -47,18 +63,6 @@ const routes = (): Router => {
         drinkName: 'Vodka'
       });
       res.status(200).json(bacChartOverTime);
-    }
-  );
-
-  router.get(
-    '/events',
-    async (req: Request, res: Response): Promise<void> => {
-      const users = await Repository.with(User).findAll({});
-      const user = new SessionUser('NONE', users[0]);
-      const session = await Session.getInstance(user);
-
-      const eventEstimates = await session.estimateEventTimes({});
-      res.status(200).json(eventEstimates);
     }
   );
 
