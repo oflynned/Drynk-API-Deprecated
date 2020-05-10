3b7a8b3a522af4178b70c7bb88c823c5f2d0c10f
Edmond O'Flynn <oflynned@tcd.ie>
2020-05-10 14:27:11 +0200

    fix multiple job dispatch with setInterval and cron running at the same time

--- src/infrastructure/cron-jobs/index.ts
+++ src/infrastructure/cron-jobs/index.ts
@@ -2,9 +2,9 @@ import { CronJob } from 'cron';
 import { bacUpdateFrequency, bacUpdateJob } from './bac-update';
 
 export const registerCronJobs = () => {
-  new CronJob(bacUpdateFrequency, () => {
+  new CronJob(bacUpdateFrequency, async () => {
     console.log('Running bac update job');
-    bacUpdateJob();
+    await bacUpdateJob();
   }).start();
 
   console.log('Registered bac update cron job');
