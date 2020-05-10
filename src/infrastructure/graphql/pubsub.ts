c04d16e86f8a87217a5b0710512f8daee4806004
Edmond O'Flynn <oflynned@tcd.ie>
2020-05-10 14:26:17 +0200

    add todo

--- src/infrastructure/graphql/pubsub.ts
+++ src/infrastructure/graphql/pubsub.ts
@@ -1,5 +1,6 @@
 import { PubSub } from 'apollo-server-express';
 
+// TODO replace this with RabbitMQ
 export const pubsub = new PubSub();
 
 export const SESSION_UPDATE_AVAILABLE = 'SESSION_UPDATE_AVAILABLE';
