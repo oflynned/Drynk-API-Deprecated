22e706730ea6071683d9056d094f7d3087a854b0
Edmond O'Flynn <oflynned@tcd.ie>
2020-05-15 17:19:53 +0200

    update e2e test

--- src/tests/infrastructure/routes/index.route.e2e.ts
+++ src/tests/infrastructure/routes/index.route.e2e.ts
@@ -3,10 +3,10 @@ import { agent } from 'supertest';
 
 const request = agent(new Server().build().app);
 
-describe('/', () => {
+describe('/health', () => {
   describe('GET', () => {
     it('should respond to health check', async () => {
-      const response = await request.get('/');
+      const response = await request.get('/health');
       expect(response.status).toEqual(200);
       expect(response.body).toEqual({ ping: 'pong' });
     });
