1522fc54a96f1a1701ee934bbc2c5aa1df6c8256
Edmond O'Flynn <oflynned@tcd.ie>
2020-05-23 14:22:05 +0200

    register cron job

--- src/infrastructure/cron-jobs/index.ts
+++ src/infrastructure/cron-jobs/index.ts
@@ -1,14 +1,24 @@
 import { CronJob } from 'cron';
-import { bacUpdateFrequency, bacUpdateJob } from './bac-update';
+import { BacUpdateJob } from './bac-update';
 import { Logger } from '../../common/logger';
+import { CleanInactiveUnonboardedUsersJob } from './clean-churned-users';
 
 const logger: Logger = Logger.getInstance('api.infrastructure.cron-jobs');
+const bacUpdateJob = new BacUpdateJob();
+const cleanUsersJob = new CleanInactiveUnonboardedUsersJob();
 
 export const registerCronJobs = () => {
-  new CronJob(bacUpdateFrequency, async () => {
+  new CronJob(bacUpdateJob.cronFrequency(), async () => {
     logger.info('Running bac update job');
-    await bacUpdateJob();
+    await bacUpdateJob.runJob();
   }).start();
 
   logger.info('Registered bac update cron job');
+
+  new CronJob(cleanUsersJob.cronFrequency(), async () => {
+    logger.info('Running clean unonboarded users job');
+    await cleanUsersJob.runJob();
+  }).start();
+
+  logger.info('Registered clean unonboarded users cron job');
 };
