4ee6bc309bdcf9b1f9bd25a7c2cfb71f0c241b18
Edmond O'Flynn <oflynned@tcd.ie>
2020-05-23 14:21:26 +0200

    add clean churned users cron job

---
+++ src/infrastructure/cron-jobs/clean-churned-users.ts
@@ -0,0 +1,20 @@
+import { CronJob } from './cron-job';
+import { User } from '../../models/user.model';
+import { Repository } from 'mongoize-orm';
+
+export class CleanInactiveUnonboardedUsersJob extends CronJob {
+  async runJob(): Promise<void> {
+    const inactiveUsers: User[] = await User.findInactive();
+    if (inactiveUsers.length === 0) {
+      return;
+    }
+
+    await Repository.with(User).hardDeleteMany({
+      _id: inactiveUsers.map((user: User) => user.toJson()._id)
+    });
+  }
+
+  cronFrequency(): string {
+    return '0 0 8 * * *';
+  }
+}
