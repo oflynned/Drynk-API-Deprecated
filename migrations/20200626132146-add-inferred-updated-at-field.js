31ad4770a73aeb1f08f25bd18d4bf407b516b49c
Edmond O'Flynn <oflynned@tcd.ie>
2020-06-26 15:24:43 +0200

    add migration

---
+++ migrations/20200626132146-add-inferred-updated-at-field.js
@@ -0,0 +1,27 @@
+const UserModel = require('../dist/models/user.model').User;
+const collection = new UserModel().collection();
+
+module.exports = {
+  async up(db, client) {
+    // already onboarded users with no updated at timestamp
+    const users = await db
+      .collection(collection)
+      .find({
+        updatedAt: undefined,
+        weight: { $exists: true },
+        height: { $exists: true },
+        sex: { $exists: true }
+      })
+      .toArray();
+
+    await Promise.all(
+      users.map(async ({ _id, createdAt }) => {
+        return db
+          .collection(collection)
+          .updateOne({ _id }, { $set: { updatedAt: createdAt } });
+      })
+    );
+  },
+
+  async down(db, client) {}
+};
