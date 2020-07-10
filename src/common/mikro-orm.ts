4c847154a6e10d36ad7aea4e90d59cb68ce40777
Edmond O'Flynn <oflynned@tcd.ie>
2020-07-10 23:18:31 +0200

    add guard on create schemas

---
+++ src/common/mikro-orm.ts
@@ -0,0 +1,26 @@
+import { bootstrapMikro } from '../config/mikro-orm.config';
+
+(async () => {
+  const orm = await bootstrapMikro();
+  const generator = orm.getSchemaGenerator();
+
+  const dropDump = await generator.getDropSchemaSQL();
+  console.log(dropDump);
+
+  const createDump = await generator.getCreateSchemaSQL();
+  console.log(createDump);
+
+  const updateDump = await generator.getUpdateSchemaSQL();
+  console.log(updateDump);
+
+  // there is also `generate()` method that returns drop + create queries
+  const dropAndCreateDump = await generator.generate();
+  console.log(dropAndCreateDump);
+
+  // or you can run those queries directly, but be sure to check them first!
+  await generator.dropSchema();
+  await generator.createSchema();
+  await generator.updateSchema();
+
+  await orm.close(true);
+})();
