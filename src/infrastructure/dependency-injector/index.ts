5b62dd6ab19eb40cbc116d7468d7ae66a3262b4f
Edmond O'Flynn <oflynned@tcd.ie>
2020-07-10 21:52:09 +0200

    create dependency injector container

---
+++ src/infrastructure/dependency-injector/index.ts
@@ -0,0 +1,26 @@
+import { bootstrapMikro } from '../../config/mikro-orm.config';
+import { Alcohol } from '../../microservices/alcohol-store/entities/alcohol.entity';
+import { EntityManager, EntityRepository, MikroORM } from 'mikro-orm';
+
+export type Container = {
+  orm: MikroORM,
+  entityManager: EntityManager,
+  alcoholRepository: EntityRepository<Alcohol>
+}
+
+export class DependencyInjector {
+  private container = {} as Container;
+
+  async registerDependents(): Promise<DependencyInjector> {
+    const orm = await bootstrapMikro();
+    this.container.orm = orm;
+    this.container.entityManager = orm.em;
+    this.container.alcoholRepository = orm.em.getRepository(Alcohol);
+
+    return this;
+  }
+
+  getContainer(): Container {
+    return this.container;
+  }
+}
