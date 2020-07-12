9123ec688a92306f1ed418b829c5e79bfa288c31
Edmond O'Flynn <oflynned@tcd.ie>
2020-07-12 14:12:54 +0200

    add session id to gql defintion for projection.gql.ts

--- src/infrastructure/graphql/definitions/types/projection.gql.ts
+++ src/infrastructure/graphql/definitions/types/projection.gql.ts
@@ -2,6 +2,7 @@ import { gql } from 'apollo-server-express';
 
 export const projectionDefinition = gql`
   type Projection {
+    sessionId: String
     startedSessionAt: ProjectionValue
     currentState: ProjectionValue
     mostDrunkAt: ProjectionValue
