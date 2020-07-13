21aade96f701e14e33a73910db205f60046d0901
Edmond O'Flynn <oflynned@tcd.ie>
2020-07-13 18:57:09 +0200

    fix sql injection vulnerability

--- src/services/alcohol.service.ts
+++ src/services/alcohol.service.ts
@@ -8,19 +8,19 @@ import { Alcohol } from '../microservices/alcohol-store/entities/alcohol.entity'
 
 export class AlcoholService {
   constructor(
-    private readonly entityManager: EntityManager,
     private readonly repo: EntityRepository<Alcohol>
-  ) {}
+  ) {
+  }
 
   async findMany(query: FilterQuery<Alcohol>): Promise<Alcohol[]> {
     return this.repo.find(query);
   }
 
   async findByName(query: string): Promise<Alcohol[]> {
-    return this.entityManager
-      .createQueryBuilder(Alcohol)
+    return this.repo
+      .createQueryBuilder()
       .select('*')
-      .where(`UPPER(name) LIKE UPPER('%${query}%')`)
+      .where(`UPPER(name) LIKE ?`, [`%${query.toUpperCase()}%`])
       .orderBy({ name: QueryOrder.ASC })
       .execute();
   }
