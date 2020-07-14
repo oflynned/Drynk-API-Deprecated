9224f7c0333ff29026f695f59515ee61b083c0ec
Edmond O'Flynn <oflynned@tcd.ie>
2020-07-14 09:39:21 +0200

    update mikro schema generator

--- src/common/mikro-orm.ts
+++ src/common/mikro-orm.ts
@@ -4,10 +4,15 @@ import { bootstrapMikro } from '../config/mikro-orm.config';
   const orm = await bootstrapMikro();
   const generator = orm.getSchemaGenerator();
 
-  await generator.getDropSchemaSQL();
-  await generator.getCreateSchemaSQL();
-  await generator.getUpdateSchemaSQL();
-  await generator.generate();
+  await generator.dropSchema();
+  await generator.createSchema();
+  await generator.updateSchema();
+  await generator.generate()
+
+  // await generator.getDropSchemaSQL();
+  // await generator.getCreateSchemaSQL();
+  // await generator.getUpdateSchemaSQL();
+  // await generator.generate();
 
   await orm.close(true);
 })();
