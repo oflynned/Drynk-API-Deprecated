c4d3528f492561f5f10b1445f86bb493cfe36c8d
Edmond O'Flynn <oflynned@tcd.ie>
2020-08-02 18:00:16 +0200

    regoster resp;vers

--- src/infrastructure/graphql/resolvers/resolvers.ts
+++ src/infrastructure/graphql/resolvers/resolvers.ts
@@ -1,11 +1,19 @@
 import { bacStateResolvers } from './subscriptions/bac-state.resolver';
 import { sessionResolvers } from './queries/session.resolver';
+import { insightsResolvers } from './queries/insights.resolver';
+import { userResolvers } from './queries/user.resolver';
+import { dateTimeTypeResolver } from './scalars.resolver';
 
 export const resolvers = {
+  ...dateTimeTypeResolver,
+
   Subscription: {
     ...bacStateResolvers
   },
+
   Query: {
-    ...sessionResolvers
+    ...userResolvers,
+    ...sessionResolvers,
+    ...insightsResolvers
   }
 };
