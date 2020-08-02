1f0cbc81578501fc349a09206d8830b19dd50095
Edmond O'Flynn <oflynned@tcd.ie>
2020-08-02 18:01:12 +0200

    split resolvers

--- src/infrastructure/graphql/definitions/index.ts
+++ src/infrastructure/graphql/definitions/index.ts
@@ -5,13 +5,40 @@ import { projectionValueDefinition } from './types/projection-value.gql';
 import { sessionDefinition } from './types/session.gql';
 import { queryDefinition } from './base/query.gql';
 import { subscriptionDefinition } from './base/subscription.gql';
+import { overviewDefinition } from './types/insights-overview.gql';
+import {
+  aggregateUnitInsightsDefinition,
+  insightsDefinition, riskGroup,
+  unitInsightsDefinition
+} from './types/insights/unit-insights.gql';
+import { userDefinition } from './types/user.gql';
+import { scalarDefinition } from './types/scalars.gql';
 
-export const typeDefs = [
-  drinkDefinition,
-  pukeDefinition,
+const insightsDefs = [
+  riskGroup,
+  insightsDefinition,
+  aggregateUnitInsightsDefinition,
+  unitInsightsDefinition,
+  overviewDefinition
+];
+
+const sessionDefs = [
   projectionDefinition,
   projectionValueDefinition,
-  sessionDefinition,
+  sessionDefinition
+];
+
+const eventDefs = [
+  drinkDefinition,
+  pukeDefinition
+];
+
+export const typeDefs = [
+  ...eventDefs,
+  ...sessionDefs,
+  ...insightsDefs,
+  userDefinition,
+  scalarDefinition,
   queryDefinition,
   subscriptionDefinition
 ];
