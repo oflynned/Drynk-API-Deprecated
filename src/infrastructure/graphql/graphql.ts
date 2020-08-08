29a7f66c18c2293c6b8d554c6b9738bb9b3ce176
Edmond O'Flynn <oflynned@tcd.ie>
2020-08-08 14:31:37 +0200

    add context to gql

--- src/infrastructure/graphql/graphql.ts
+++ src/infrastructure/graphql/graphql.ts
@@ -4,14 +4,25 @@ import { Server as HttpServer, createServer } from 'http';
 import { typeDefs } from './definitions';
 import { resolvers } from './resolvers/resolvers';
 import { Environment } from '../../config/environment';
+import { authGqlContext, GqlContext } from '../middleware/identity.middleware';
 
 export const graphql = (app: Application): HttpServer => {
   const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
     playground: Environment.isDevelopment(),
-    introspection: Environment.isDevelopment()
+    introspection: Environment.isDevelopment(),
+    context: async ({ req, connection }) => {
+      if (connection && connection.context) {
+        // subscription context
+        return authGqlContext({ headers: { ...connection.context } } as any);
+      }
+
+      // request context
+      return authGqlContext(req);
+    }
   });
+
   apolloServer.applyMiddleware({ app });
 
   const httpServer = createServer(app);
