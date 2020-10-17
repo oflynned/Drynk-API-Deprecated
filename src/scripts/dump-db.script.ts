92bcb36e114fd2ed919e45b35243e93abc596c12
Edmond O'Flynn <oflynned@tcd.ie>
2020-10-17 17:41:17 +0200

    update some db dump scripts

--- src/scripts/dump-db.script.ts
+++ src/scripts/dump-db.script.ts
@@ -6,7 +6,22 @@ class DumpDb {
   async run(): Promise<void> {
     const uri = process.env.BACKUP_FROM_URI;
     exec(`yarn db:dump:clean && mkdir -p dump`);
+
+    // this.fetchCollectionAsBson(uri);
+
+    ['users', 'drinks', 'sessions'].forEach(collection => {
+      this.fetchCollectionAsJson(uri, collection);
+    });
+  }
+
+  fetchCollectionAsBson(uri: string): void {
     exec(`mongodump --uri ${uri} --out ./dump`);
+  }
+
+  fetchCollectionAsJson(uri: string, collection: string): void {
+    exec(
+      `mongoexport --uri ${uri} --collection ${collection} --jsonArray --pretty --out ./dump/${collection}.json`
+    );
   }
 }
 
